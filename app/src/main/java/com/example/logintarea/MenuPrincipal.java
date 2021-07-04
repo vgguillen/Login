3841a6a5bdc96e95a3449c8003a856cb0beaa1cb
vgguillen <victor.guillen2015@uteq.edu.ec>
2021-07-03 20:22:44 -0500

    Navegación

--- app/src/main/java/com/example/logintarea/MenuPrincipal.java
+++ app/src/main/java/com/example/logintarea/MenuPrincipal.java
@@ -1,13 +1,22 @@
 package com.example.logintarea;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+
+import org.jetbrains.annotations.NotNull;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -27,5 +36,17 @@ public class MenuPrincipal extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.navigationView);
         navigationView.setItemIconTintList(null);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        final TextView textTitle= findViewById(R.id.textTitle);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull @NotNull NavController controller, @NonNull @NotNull NavDestination destination, @Nullable @org.jetbrains.annotations.Nullable Bundle arguments) {
+                textTitle.setText(destination.getLabel());
+            }
+        });
     }
 }
